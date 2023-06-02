ea176e11056e1059222771086cee5a262984622d
MOHAMED FAIZAL M.S <108245146+faizalmsdev@users.noreply.github.com>
2023-06-02 09:40:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll('.square');
+
+squares.forEach(square => {
+  square.addEventListener('mouseover', () => {
+    square.style.transition = 'background-color 1s';
+    square.style.backgroundColor = '#ff0000';
+  });
+
+  square.addEventListener('mouseout', () => {
+    square.style.transition = 'background-color 1s';
+    setTimeout(() => {
+      square.style.backgroundColor = '#ccc';
+    }, 1000);
+  });
+});
